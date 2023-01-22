dcdf10817cee46d20b523be0f98bf3f751303de1
amartyapandey7681 <amartyapandey49@gmail.com>
2023-01-22 19:14:40 +0530

    spotify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App(){
+
+  return (<div className='songs'>
+
+  <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4npDJDFDYLg?utm_source=generator" title="alan" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO4nBpII?utm_source=generator" title="green" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO2YqUuI?utm_source=generator" title="imagine" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO2LLaKs?utm_source=generator" title="hardy" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWxPM4nWdhyI?utm_source=generator" title="alan" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  </div>);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <div className='top'>
+    <h2>Playlists</h2>
     <App />
+    <h3>. . . . . .</h3>
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
